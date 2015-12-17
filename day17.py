421dfae8dc2a977cd4a05ef6f832d63f077b0a04
david.nemeth.csoka <david.nemeth.csoka@prezi.com>
2015-12-17 08:08:45 +0100

    cleanup

--- day17.py
+++ day17.py
@@ -6,10 +6,10 @@ def get_combinations(containers, target):
 	elif target < 0 or len(containers) == 0:
 		return
 	else:
-		for res in get_combinations(containers[1:], target):
-			yield res
-		for res in get_combinations(containers[1:], target - containers[0]):
-		 	yield [containers[0]] + res
+		for combination in get_combinations(containers[1:], target):
+			yield combination
+		for combination in get_combinations(containers[1:], target - containers[0]):
+		 	yield [containers[0]] + combination
 	
 containers = []
 for line in fileinput.input():
