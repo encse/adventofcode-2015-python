85fab2092e715b02ac2797d9c9b10ec67621feaf
david.nemeth.csoka <david.nemeth.csoka@prezi.com>
2015-12-18 15:37:37 +0100

    shorter

--- day18.py
+++ day18.py
@@ -1,58 +1,33 @@
-import fileinput
+import sys
+import itertools as it
+import copy
+
+def dup(tbl): 
+	return copy.deepcopy(tbl)
 
 def lit_corners(tbl):
-	tblNew = []
-	
-	for irow in xrange(0, crow):
-		tblNew.append(list(tbl[irow]))
-
-	tblNew[0][0] = tblNew[0][ccol-1] = tblNew[crow-1][0] = tblNew[crow-1][ccol-1] = '#'
-	return tblNew
+	tbl[0][0] = tbl[0][ccol-1] = tbl[crow-1][0] = tbl[crow-1][ccol-1] = '#'
+	return tbl
 
 def step(tbl):
-	tblNew = []
+	tblNew = dup(tbl)
 	
-	for irow in xrange(0, crow):
-		row = []
-		for icol in xrange(0, ccol):
-			c = 0
-			if icol-1 >= 0 and irow-1 >= 0 and tbl[irow -1][icol-1] == '#': c+=1
-			if irow-1 >= 0 and tbl[irow -1][icol] == '#': c+=1
-			if icol+1 < ccol and irow-1 >= 0 and tbl[irow -1][icol+1] == '#': c+=1
-			
-			if icol-1 >= 0 and tbl[irow][icol-1] == '#': c+=1
-			if icol+1 < ccol and tbl[irow][icol+1] == '#': c+=1
-			
-			if icol-1 >= 0 and irow+1 < crow and tbl[irow +1][icol-1] == '#': c+=1
-			if irow+1 < crow and tbl[irow +1][icol] == '#': c+=1
-			if icol+1 < ccol and irow+1 < crow and tbl[irow +1][icol+1] == '#': c+=1
-
-			if tbl[irow][icol] == '#' :
-				row .append('#' if c ==2 or c ==3 else '.')
-			elif tbl[irow][icol] == '.':
-				row .append('#' if c == 3 else '.')
-			
-		tblNew.append(row)
+	for (irow, icol, ch) in [(irow, icol, tbl[irow][icol]) for irow in xrange(0, crow) for icol in xrange(0, ccol)]:
+		c_neighbour = 0
+		for (dx,dy) in [(-1,-1), (0,-1), (1,-1), (-1,0), (1,0), (-1,1), (0,1), (1,1)]:
+			(icolN, irowN) = (icol + dx, irow +dy)
+			if icolN >= 0 and icolN < ccol and irowN >= 0 and irowN < crow and tbl[irowN][icolN] == '#': 
+				c_neighbour+=1
+		tblNew[irow][icol] = {'#': '..##.....', '.': '...#.....'}[ch][c_neighbour]
 	return tblNew
-tbl1 = []
-for line in fileinput.input():
-	tbl1.append(list(line.strip()))
-crow = len(tbl1)
-ccol = len(tbl1[0])
 
-tbl2 = lit_corners(tbl1)
+tbl1 = map(lambda line: list(line.strip()), sys.stdin.readlines())
+(crow, ccol) = (len(tbl1), len(tbl1[0]))
+tbl2 = lit_corners(copy.deepcopy(tbl1))
 
 for i in xrange(0,100):
 	tbl1 = step(tbl1)
 	tbl2 = lit_corners(step(tbl2))
 
-s1 = 0
-s2 = 0
-for irow in xrange(0, crow):
-	for icol in xrange(0, ccol):
-		if tbl1[irow][icol] == '#':
-			s1 += 1
-		if tbl2[irow][icol] == '#':
-			s2 += 1
-print s1
-print s2
+print list(it.chain(*tbl1)).count('#')
+print list(it.chain(*tbl2)).count('#')
