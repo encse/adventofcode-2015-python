cb70de3136acc044389b676b1d3892588a6c7417
encse <encse@csokavar.hu>
2015-12-19 14:36:12 +0100

    cleanup

--- day19.py
+++ day19.py
@@ -3,19 +3,15 @@ import sys
 def apply_rule(molecule, rule, i): 
 	return molecule[:i] + rule[1] + molecule[(i+len(rule[0])):] 
 
-def match(molecule, rule, i): 
-	return molecule.find(rule[0], i, i+len(rule[0])) == i
+def get_matching_rule_at(molecule, rules, i): 
+	return (rule for rule in rules if rule[0] == molecule[i:i+len(rule[0])])
 
-def get_first_match(molecule, rules): 
-	return next ((i for i in xrange(len(molecule)) for rule in rules if match(molecule, rule, i)), None)
+def get_matching_positions(molecule, rules): 
+	return (i for i in xrange(len(molecule)) if any(get_matching_rule_at(molecule, rules, i)))
 
 def get_next_molecules(molecule, rules, lookahead):
-	i = get_first_match(molecule, rules)
-	if i == None: return []
-	return set([apply_rule(molecule, rule, j)
-			for j in xrange(i, i+lookahead)
-			for rule in rules 
-			if match(molecule, rule, j)])
+	i = next(get_matching_positions(molecule, rules))
+	return (apply_rule(molecule, rule, j) for j in xrange(i, i + lookahead) for rule in get_matching_rule_at(molecule, rules, j))
 	
 rules = []
 for line in sys.stdin:
@@ -25,7 +21,7 @@ for line in sys.stdin:
 		break
 	rules.append(line.split(" => "))
 
-print len(get_next_molecules(goal, rules, len(goal)))
+print len(set(get_next_molecules(goal, rules, len(goal))))
 
 lookahead = max(map(lambda rule: len(rule[1]), rules)) # longest rule
 rules = map(lambda rule: rule[::-1], rules)	# we go backwards so change the left and right side of the rules
@@ -33,6 +29,6 @@ rules = map(lambda rule: rule[::-1], rules)	# we go backwards so change the left
 molecules = {goal}
 i = 0
 while not 'e' in molecules:
-	molecules = set([next_molecule for molecule in molecules for next_molecule in get_next_molecules(molecule, rules, lookahead)])
+	molecules = set((next_molecule for molecule in molecules for next_molecule in get_next_molecules(molecule, rules, lookahead)))
 	i += 1
 print i
