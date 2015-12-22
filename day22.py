fc31aa6bd9d943f8755213d9c701b66191272c89
encse <encse@csokavar.hu>
2015-12-22 14:35:15 +0100

    cleanup

--- day22.py
+++ day22.py
@@ -1,126 +1,89 @@
-import copy
 import itertools as it
-
-def no_log(*args):
-	pass
-def do_log(*args):
-	print ' '.join(map(str, args))
+import sys
 
 class Spell:
-	time = 0
-	def cast(self, player, other_player):
-		log('{0} casts {1}.'.format(player.name, self.__class__.__name__))
-		pass
-	
-	def effect_over_time(self, player, other_player):
-		log('{0}\'s timer is now {1}.'.format(self.__class__.__name__, self.time))
-		pass
+	time, damage, mana = 0, 0, 0
+	def cast(self, player, other_player): pass
+	def effect_over_time(self, player, other_player): pass
+	def wear_off(self, player, other_player): pass
+	def dup(self):
+		clone = self.__class__()
+		clone.time = self.time
+		return clone
 
-	def wear_off(self, player, other_player):
-		log('{0} wears off'.format(self.__class__.__name__))
-		pass
 
 class Attack(Spell):
-
 	def cast(self, player, other_player):
-		other_player.do_damage(self.damage)
-		if other_player.armor > 0:
-			log('Boss attacks for', self.damage, '-', other_player.armor,'=', self.damage - other_player.armor, 'damage!')
-			pass
-		else:
-			log('Boss attacks for', self.damage, 'damage!')
-			pass
-Attack.mana = 0
+		other_player.damage(self.damage)
+
 
 class MagicMissile(Spell):
+	mana, damage = 53, 4
 	def cast(self, player, other_player):
-		d = other_player.do_damage(self.damage)
-		log('Player casts Magic Missile, dealing {0} damage.'.format(d))
-MagicMissile.mana = 53
-MagicMissile.damage = 4
+		other_player.damage(self.damage)
+
 
 class Drain(Spell):
+	mana, damage = 73, 2
 	def cast(self, player, other_player):
-		d = player.increase_hp(self.damage)
-		other_player.do_damage(self.damage)
-		log('Player casts Drain, dealing {0} damage, and healing {1} hit points.'.format(self.damage, d))
-		pass
-Drain.mana = 73
-Drain.damage = 2
+		player.heal(self.damage)
+		other_player.damage(self.damage)
+
 
 class Shield(Spell):
-	time = 6
+	time, mana = 6, 113
 	def cast(self, player, other_player):
 		player.armor += 7
-		log(player.name, 'casts Shield, increasing armor by 7.')
-		pass
 
 	def wear_off(self, player, other_player):
 		player.armor -= 7
-		log('Shield wears off, decreasing armor by 7.')
-		pass
 
-Shield.damage = 0
-Shield.mana = 113
 
 class Poison(Spell):
-	time = 6
+	time, mana, damage = 6, 173, 3
 	def effect_over_time(self, player, other_player):
-		other_player.do_damage(Poison.damage)
-		log('Poison deals {0} damage; its timer is now {0}.'.format(self.damage, self.time))
-		pass
+		other_player.damage(Poison.damage)
 
-Poison.mana = 173
-Poison.damage = 3
 
 class Recharge(Spell):
-	time = 5
+	time, mana = 5, 229
 	def effect_over_time(self, player, other_player):
 		player.mana += 101
-		log('Recharge provides 101 mana; its timer is now {0}.'.format(self.time))
-		pass
 
-Recharge.mana = 229
-Recharge.damage = 0
 
 class Player:
-	def __init__(self, name, base_hp, hp, base_armor, armor, base_mana, mana, spell_book, active_spells):
-		self.name = name
-		self.base_hp = base_hp
-		self.hp = hp
-		self.base_armor = base_armor
-		self.armor =armor
-		self.base_mana = base_mana
-		self.mana = mana
+	def __init__(self, base_hp, base_mana, spell_book):
+		self.hp = self.base_hp = base_hp
+		self.armor = self.base_armor = 0
+		self.mana = self.base_mana = base_mana
 		self.spell_book = spell_book
-		self.active_spells = active_spells
+		self.active_spells = []
 		self.mana_spent = 0
+	
+	def dup(self):
+		clone = Player(self.base_hp, self.base_mana, self.spell_book) 
+		clone.mana_spent = self.mana_spent
+		clone.mana = self.mana
+		clone.hp = self.hp
+		clone.active_spells = map(lambda x: x.dup(), self.active_spells)
+		clone.base_armor = self.base_armor
+		clone.armor = self.armor
+		return clone
+	
+	def is_wizard(self): 
+		return self.base_mana > 0
+	
+	def damage(self, d): 
+		self.hp -= max(1, d - self.armor)
 
-	def do_damage(self, d):
-		d -= self.armor
-		if d  <= 0:
-			d = 1
-
-		self.hp -= d
-		return d 
-
-	def increase_hp(self, d):
-		if self.hp + d > self.base_hp:
-			d = self.base_hp - self.hp
-		self.hp += d
-		return d
+	def heal(self, d): 
+		self.hp = min(self.base_hp, self.hp + d)
 
 	def cast(self, spell, other_player):
 		spell.cast(self, other_player)
-		if self.mana < spell.mana:
-			raise 'coki'
-		self.mana -= spell.mana
-		self.mana_spent += spell.mana
+		self.mana, self.mana_spent = self.mana - spell.mana, self.mana_spent + spell.mana
 		if spell.time > 0:
 			self.active_spells.insert(0, spell)
-
-	def dup(self):
-		return copy.deepcopy(self)
 
 	def get_steps(self):
 		for Spell in self.spell_book:
@@ -133,95 +96,37 @@ class Player:
 			if not is_active and self.mana >= Spell.mana:
 				yield Spell()
 
-	def __str__(self):
-		if self.base_mana > 0:
-			return '{0} has {1} hit points, {2} armor, {3} mana'.format(self.name, self.hp, self.armor, self.mana)
-		else:
-			return '{0} has {1} hit points'.format(self.name, self.hp)
+def strategies(player, boss, is_hard):
+	
+	def do_spells(player, other_player):
+		for spell in list(player.active_spells):
 
-def wizard(p1, p2):
-	return p1 if p1.base_mana > 0 else p2
-
-def warrior(p1, p2):
-	return p1 if p1.base_mana == 0 else p2
-
-def do_spells(player, other_player):
-	for spell in list(player.active_spells):
-
-		if spell.time > 0:
 			spell.time -= 1
 			spell.effect_over_time(player, other_player)
-	
-		if spell.time == 0:
-			spell.wear_off(player, other_player)
-			player.active_spells.remove(spell)
-			
-def init_round(player, other_player):
+		
+			if spell.time == 0:
+				spell.wear_off(player, other_player)
+				player.active_spells.remove(spell)
 
-		do_spells(player, other_player)
-		do_spells(other_player, player)
-		return player, other_player
-	
-def turn(p1, p2, step):
-	log('--', p1.name, 'turn --')
-	log('-', wizard(p1,p2))
-	log('-', warrior(p1,p2))
-	(p1, p2) = init_round(p1, p2)
-
-	if p1.hp > 0 and p2.hp >0:
-		p1.cast(step, p2)
-
-	return (p1, p2)
-
-player_hp, player_mana, boss_hp, Attack.damage = 50, 500, 71, 10
-# player_hp, player_mana, boss_hp, Attack.damage = 10, 250, 13, 8
-# player_hp, player_mana, boss_hp, Attack.damage = 10, 250, 14, 8
-
-player = Player('Player', player_hp,player_hp,0,0,player_mana,player_mana,[MagicMissile, Drain, Shield, Poison, Recharge], [])
-boss = Player('Boss', boss_hp,boss_hp,0,0,0,0,[Attack], [])
-
-def foo(player, boss, hard):
-	print 'foo'
-	i=0
-	states = [(player, boss, [])]
+	states = [(player, boss)]
 	while len(states) > 0:
-		playerOrig, bossOrig, prev_steps = states[0]
-		states = states[1:] 
-		i+=1
+		p1_orig, p2_orig = states.pop()
 		
-		for step in playerOrig.get_steps():
-			player = playerOrig.dup()
-			boss = bossOrig.dup()
-			if hard:
-				player.hp -=1
-				if player.hp <= 0:
-					continue
+		for step in p1_orig.get_steps():
+			p1, p2 = p1_orig.dup(), p2_orig.dup()
+			
+			if is_hard and p1.is_wizard():	p1.hp -= 1
+			if p1.hp > 0 and p2.hp > 0: do_spells(p1, p2)
+			if p1.hp > 0 and p2.hp > 0: do_spells(p2, p1)
+			if p1.hp > 0 and p2.hp > 0:	p1.cast(step, p2)
+			if p1.hp > 0 and p2.hp > 0:	states.append((p2, p1))
+			
+			if p1.is_wizard() and p1.hp > 0 and not p2.hp > 0: yield p1.mana_spent
+			if p2.is_wizard() and p2.hp > 0 and not p1.hp > 0: yield p2.mana_spent
 
-			turn(player, boss, step)
-			if player.hp > 0 and boss.hp <= 0:
-				yield player.mana_spent, list(prev_steps) + [step]
-			elif player.hp > 0 and boss.hp > 0:
-				boss_steps = list(boss.get_steps())
-				if len(boss_steps) != 1:
-					raise 'wtf'
-				boss_step = boss_steps[0]
-				turn(boss, player, boss_step)
-				if player.hp > 0 and boss.hp <= 0:
-					yield player.mana_spent, list(prev_steps) + [step]
-				elif player.hp > 0:
-					states.append((player, boss, list(prev_steps) + [step]))
-		
-		if (i % 10000 == 0):
-			print len(states)
-			print playerOrig
-			print bossOrig
-			print ''
-			states = sorted(states, key=lambda(p1,p2,_):p2.mana_spent)
-		log('')
+player = Player(50,500,[MagicMissile, Drain, Shield, Poison, Recharge])
+boss = Player(71,0,[Attack])
+Attack.damage = 10
 
-log = no_log
-for mana, steps in foo(player, boss, True):
-	log = do_log
-	print mana
-	print steps
-	break
+print min(strategies(player, boss, False))
+print min(strategies(player, boss, True))
