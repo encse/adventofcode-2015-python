becbd02e497f69246284daa1bb01bd570b882f0b
encse <encse@csokavar.hu>
2015-12-24 13:12:41 +0100

    day24

--- day24/solution.py
+++ day24/solution.py
@@ -1,39 +1,39 @@
 import sys
-from operator import mul
-def product(list): return reduce(mul, list, 1)
+import math
 
-def pick(max_len, w, ps):
-	if w == 0:
-		yield ([],ps)
-	elif max_len == 0:
-		return
-	elif len(ps) == 0:
-		return
-	else:
-		if ps[0] > w:
+def product(list):
+	return reduce(lambda x, y : x * y, list, 1)
+
+def pick(max_count, sum_weight, packages):
+	if sum_weight == 0:
+		yield ([],packages)
+	elif max_count > 0 and len(packages) > 0:
+		if packages[0] > sum_weight:
 			return
-		for (r1, r2) in pick(max_len - 1, w - ps[0], ps[1:]):
-			yield ([ps[0]] + r1, r2)
-		for (r1, r2) in pick(max_len, w, ps[1:]):
-			yield (r1, [ps[0]] + r2) 
+		for (packages0, packages1) in pick(max_count - 1, sum_weight - packages[0], packages[1:]):
+			yield ([packages[0]] + packages0, packages1)
+		for (packages0, packages1) in pick(max_count, sum_weight, packages[1:]):
+			yield (packages0, [packages[0]] + packages1) 
 
+def splittable(part_count, weight, packages):
+	if part_count == 1: 
+		return True if sum(packages) == weight else False
+	else:
+		for (packages0, packages1) in pick(len(packages), weight, packages):
+			if splittable(part_count - 1, weight, packages1):
+				return True
+	return False
 
-weights = map(int, sys.stdin.read().split('\n'))
-w = sum(weights)/4
-print w, len(weights)
-i = 0
+def solve(part_count, packages):
+	weight = sum(packages) / part_count
+	for min_length in xrange(len(packages)):
+		minQ = float('inf')
+		for (packages0, packages1) in pick(min_length, weight, packages):
+			if splittable(part_count - 1, weight, packages1):
+				minQ = min (minQ, product(packages0))
+		if not math.isinf(minQ):
+			return minQ
 
-minQ = product(weights) * 2
-minL = len(weights) * 2
-
-for (p1, w1) in pick(4, w, weights):
-	for (p2, w2) in pick(len(weights), w, w1):
-		for (p3, p4) in pick(len(weights), w, w2):
-			i+=1
-			if len(p1) < minL:
-				minL = len(p1)
-				minQ = product(p1)
-			elif len(p1) == minL:
-				minQ = min (minQ, product(p1))
-			break	
-print i, minL, minQ
+packages = map(int, sys.stdin.read().split('\n'))
+print solve(3, packages)
+print solve(4, packages)
